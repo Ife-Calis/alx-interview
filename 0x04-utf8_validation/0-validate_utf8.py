4b314fd17987eb9849459089f14f8fa64fd4e0db
Ife-Calis <calistusifechukwu39@gmail.com>
2024-05-23 11:24:48 +0000

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python3
-"""
-Determines if a given data set represents a valid UTF-8 encoding
-"""
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
+    """Determines if a given data set
+    represents a valid utf-8 encoding
     """
-    Validates UTF-8 encoding
+    number_bytes = 0
 
-    Args:
-        data (list): A dataset represented by a list of integers
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-    Return:
-        True if data is a valid UTF-8 encoding, else False
-    """
+    for i in data:
 
-    byte_count = 0
+        mask_byte = 1 << 7
 
-    for byte in data:
-        if byte_count == 0:
-            if (byte >> 7) == 0b0:
-                # Single byte character, no need for further checks
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
                 continue
-            elif (byte >> 5) == 0b110:
-                byte_count = 1
-            elif (byte >> 4) == 0b1110:
-                byte_count = 2
-            elif (byte >> 3) == 0b11110:
-                byte_count = 3
-            else:
-                return False
-        else:
-            if (byte >> 6) != 0b10:
-                return False
-            byte_count -= 1
 
-    return byte_count == 0
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
