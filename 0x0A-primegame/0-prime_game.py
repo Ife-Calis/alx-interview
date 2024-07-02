58ad93986d750d69e605a71e97d1c667243a420d
Ife-Calis <calistusifechukwu39@gmail.com>
2024-07-02 09:12:59 +0000

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,90 @@
 #!/usr/bin/python3
 """
-Module: Game of choosing Prime numbers
+Prime game
 """
 
 
-def primeNumbers(n):
-    """Return list of prime numbers between 1 and n inclusive
-       Args:
-        n (int): upper boundary of range. lower boundary is always 1
+def is_prime(n):
     """
-    primeNos = []
-    filtered = [True] * (n + 1)
-    for prime in range(2, n + 1):
-        if (filtered[prime]):
-            primeNos.append(prime)
-            for i in range(prime, n + 1, prime):
-                filtered[i] = False
-    return primeNos
+    Checks if number is a prime
+
+    Args:
+        n (int): Number
+
+    Return:
+        True if n is a prime, else False
+    """
+    if n < 2:
+        return False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def prime_numbers(start, end):
+    """
+    Returns a list of prime numbers from start to end (inclusive)
+
+    Args:
+        start (int): Start number
+        end (int): Last number to include
+    """
+    primes = [n for n in range(start, end + 1) if is_prime(n)]
+    return primes
 
 
 def isWinner(x, nums):
     """
-    Determines winner of Prime Game
+    Determines the winner of a prime numbers game
+
     Args:
-        x (int): no. of rounds of game
-        nums (int): upper limit of range for each round
+        x (int): The number of rounds
+        nums (list): An array
+
     Return:
-        Name of winner (Maria or Ben) or None if winner cannot be found
+        name of player who won the most rounds
+        None, if the winner cannot be determined
     """
-    if x is None or nums is None or x == 0 or nums == []:
-        return None
-    Maria = Ben = 0
-    for i in range(x):
-        primeNos = primeNumbers(nums[i])
-        if len(primeNos) % 2 == 0:
-            Ben += 1
-        else:
-            Maria += 1
-    if Maria > Ben:
-        return 'Maria'
-    elif Ben > Maria:
-        return 'Ben'
-    return None
+
+    winner = None
+
+    if not nums or x < 1:
+        return winner
+
+    mariaCount = 0
+    benCount = 0
+
+    for num in nums:
+        rounds = list(range(1, num + 1))
+        primes = prime_numbers(1, num)
+
+        if not primes:
+            benCount += 1
+            continue
+
+        maria_turn = True
+
+        while (True):
+            if not primes:
+                if maria_turn:
+                    benCount += 1
+                else:
+                    mariaCount += 1
+                break
+
+            leastPrime = primes.pop(0)
+            rounds.remove(leastPrime)
+
+            rounds = [x for x in rounds if x % leastPrime != 0]
+
+            maria_turn = not maria_turn
+
+    if mariaCount > benCount:
+        winner = "Maria"
+
+    if benCount > mariaCount:
+        winner = "Ben"
+
+    return winner
